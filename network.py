68e8b27140638f2279eb32242e108f8d088cc7d3
AadityaSRV <adityasrivastava9567@gmail.com>
2025-08-26 18:06:22 +0530

    Neural Network

--- network.py
+++ network.py
@@ -9,21 +9,50 @@ def sigmoid(x):
     
 class Neuron:
     
-    def __init__(self,weight,bias):
-        self.weight = weight
+    def __init__(self,weights,bias):
+        self.weights = weights
         self.bias = bias
         
-    def feed_forward(self,input):
-        total = np.dot(self.weight,input)+self.bias
+    def feed_forward(self,inputs):
+        total = np.dot(self.weights,inputs)+self.bias
         return sigmoid(total)
 
 
-weight = np.array([0,1])
-bias =4
-n = Neuron(weight,bias)
 
-x=np.array([2,3])
-print(n.feed_forward(x))    
+class NeuralNetwork:
+
+#assume all neurons have the same  weights w= [0,1],  the same bias b=0 and 
+# the same sigmoid activation function. Let h1,h2,o1 denote the outputs of the neurons they represent.
+    '''
+  A neural network with:
+    - 2 inputs
+    - a hidden layer with 2 neurons (h1, h2)
+    - an output layer with 1 neuron (o1)
+  Each neuron has the same weights and bias:
+    - w = [0, 1]
+    - b = 0
+    '''     
+    def __init__(self):
+        weights = np.array([0,1])
+        bias =0
+
+        self.h1 = Neuron(weights,bias)
+        self.h2 = Neuron(weights,bias)
+        self.o1 = Neuron(weights,bias)
+
+    def feedforward(self,x):
+        out_h1 = self.h1.feed_forward(x)
+        out_h2 = self.h2.feed_forward(x)
+
+        out_o1 = self.o1.feed_forward(np.array([out_h1,out_h2]))
+        return out_o1
+        
+network = NeuralNetwork()
+x= np.array([2,3])
+print(network.feedforward(x))
+
+
+
 
 
 
