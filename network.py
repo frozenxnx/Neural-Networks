f00e7d56e45ce50d762d1705e48caabd573af5d3
AadityaSRV <adityasrivastava9567@gmail.com>
2025-08-26 15:38:58 +0530

    first commit

--- network.py
+++ network.py
@@ -1,12 +1,31 @@
 import numpy as np
 
-class Outernetwok:
 
-    #Implementation of a neural network
+    #Implementation of a single neuron
 
-    def sigmoid(x):
-        return 1+1/(1+np.exp(-x))
+def sigmoid(x):
+    # Our activation function 
+    return 1/(1+np.exp(-x))
     
+class Neuron:
     
-    def 
+    def __init__(self,weight,bias):
+        self.weight = weight
+        self.bias = bias
         
+    def feed_forward(self,input):
+        total = np.dot(self.weight,input)+self.bias
+        return sigmoid(total)
+
+
+weight = np.array([0,1])
+bias =4
+n = Neuron(weight,bias)
+
+x=np.array([2,3])
+print(n.feed_forward(x))    
+
+
+
+
+
