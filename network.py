f2d5df6008ae08084a76f0095671c570495d7082
AadityaSRV <adityasrivastava9567@gmail.com>
2025-08-30 17:13:44 +0530

    Updated Neural Network

--- network.py
+++ network.py
@@ -7,22 +7,14 @@ def sigmoid(x):
     # Our activation function 
     return 1/(1+np.exp(-x))
     
-class Neuron:
-    
-    def __init__(self,weights,bias):
-        self.weights = weights
-        self.bias = bias
-        
-    def feed_forward(self,inputs):
-        total = np.dot(self.weights,inputs)+self.bias
-        return sigmoid(total)
+def der_sigmoid(x):
+    fx = sigmoid(x)
+    return fx * (1-fx)
 
-
+def mse_loss(y_true,y_pred):
+    return ((y_true-y_pred)**2).mean()
 
 class NeuralNetwork:
-
-#assume all neurons have the same  weights w= [0,1],  the same bias b=0 and 
-# the same sigmoid activation function. Let h1,h2,o1 denote the outputs of the neurons they represent.
     '''
   A neural network with:
     - 2 inputs
@@ -33,23 +25,54 @@ class NeuralNetwork:
     - b = 0
     '''     
     def __init__(self):
-        weights = np.array([0,1])
-        bias =0
+     
+         
+        #weights
+        self.w1 = np.random.normal()
+        self.w2 = np.random.normal()
+        self.w3 = np.random.normal()
+        self.w4 = np.random.normal()
+        self.w5 = np.random.normal()
+        self.w6 = np.random.normal()
 
-        self.h1 = Neuron(weights,bias)
-        self.h2 = Neuron(weights,bias)
-        self.o1 = Neuron(weights,bias)
+        #biases
+        self.b1 = np.random.normal()
+        self.b2 = np.random.normal()
+        self.b3 = np.random.normal()                    
+   
 
     def feedforward(self,x):
-        out_h1 = self.h1.feed_forward(x)
-        out_h2 = self.h2.feed_forward(x)
+        h1 = sigmoid(self.w1*x[0] + self.w2*x[1] + self.b1)
+        h2 = sigmoid(self.w3*x[0] + self.w4*x[1] + self.b2)
+        o1 = sigmoid(self.w5*h1 + self.w6*h2 + self.b3)
+        return o1
+    
 
-        out_o1 = self.o1.feed_forward(np.array([out_h1,out_h2]))
-        return out_o1
+    def train(self,data,all_y_trues):
+        """
+
+        """
+    
+
+
         
-network = NeuralNetwork()
-x= np.array([2,3])
-print(network.feedforward(x))
+
+        
+        
+data = np.array(
+        [-2,-1]
+        [25,6]
+        [17,4]
+        [-15,-6])
+
+all_y_trues = np.array(
+    [1,
+    0,
+    0,
+    1,
+    ]
+    )
+
 
 
 
